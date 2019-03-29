d01375eb89025af8943c2b6bd07fc2f2f4e7f00d
nedmorgan <nmorgan1987@gmail.com>
2019-03-29 11:44:18 -0400

    used different math functions in Go

--- hello.go
+++ hello.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 )
 
 func main() {
 	fmt.Println("My favorite number is ", rand.Intn(100))
+	fmt.Println("The square root of 7 is", math.Sqrt(7))
+	fmt.Printf("Now you have %g problems.", math.Sqrt(8))
 }
